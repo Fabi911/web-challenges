a380fa4b5a32cad2b80a5e73f0c124bdf00bdf31
Fabian Döz <fabian@doez.info>
2024-02-08 10:57:12 +0100

    upload css_rep playground

--- js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -1,15 +1,15 @@
 const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
-const output = document.querySelector('[data-js="age-output"]');
+const text = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
 if(age >=13 && age < 20){
-  output.innerHTML='You are a teen.'
+  text.innerHTML='You are a teen.'
 } else {
-  output.innerHTML='You are not a teen.'
+  text.innerHTML='You are not a teen.'
 };
   // Exercise:
   // Use conditions and the logical AND operator to write
