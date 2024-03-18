89bb1b4788c7bebd553b1d7a9c8b429c89c4db40
Fabian Döz <fabian@doez.info>
2024-03-18 12:11:41 +0100

    finish put Task

---
+++ backend/backend-update-and-delete_products/components/Product/index.js
@@ -0,0 +1,61 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
+      <StyledLink href="/">Back to all</StyledLink>
+      <button
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </button>
+    </ProductCard>
+  );
+}
